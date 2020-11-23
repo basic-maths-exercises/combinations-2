9b022107c3cc9fa83fe75b3c9d94f86778f5b63e
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-23 15:31:32 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+def gencombinations( N, R ) : 
+  if sum(R)!=N : 
+    print("error in counts of numbers of combinations ")
+  allcombinations = []
+  # Your code goes here 
+  
+  return allcombinations
+
+# This is test code 
+print( gencombinations(5,[1,2,2]) )
+
+
